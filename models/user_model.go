c143b8f8e90a0f714517804dd688290267fcf3b7
sidx04 <siddpal04@gmail.com>
2023-12-03 19:52:03 +0530

    fix: removed ID which was breaking /:id endpoint

--- models/user_model.go
+++ models/user_model.go
@@ -1,15 +1,12 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 /*
 Create a User struct with required properties.
 We added omitempty and validate:"required" to the struct tag to tell Gin to ignore
 empty fields and make the field required, respectively.
 */
 type User struct {
-	Id       primitive.ObjectID `json:"id,omitempty"`
-	Name     string             `json:"name,omitempty" validate:"required"`
-	Location string             `json:"location,omitempty" validate:"required"`
-	Title    string             `json:"title,omitempty" validate:"required"`
+	Name     string `json:"name,omitempty" validate:"required"`
+	Location string `json:"location,omitempty" validate:"required"`
+	Title    string `json:"title,omitempty" validate:"required"`
 }
